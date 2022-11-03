243e5e828c095dacab2460a2223ee4aaccd6d376
Evi Vermeêren <evivermeeren@hotmail.com>
2022-11-03 17:15:17 +0100

    tweede commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.head}>
+        
+        <View>
+          <Text style={styles.title}>Bookshop</Text>
+        </View>
+
+        <View style={styles.shoppingcart}>
+          <Image
+              style={styles.icon}
+              source={{uri: 'https://cdn-icons-png.flaticon.com/512/1413/1413908.png'}}
+            />
+          <Text style={styles.text}>1</Text>
+        </View>
+
+      </View>
+      
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -12,11 +30,43 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 75,
   },
+
+  head: {
+    flex: 1,
+    flexDirection: 'row',
+    flexGrow: 0, 
+    flexShrink: 0,
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginLeft: 25,
+  },
+
+  title: {
+    fontSize: 25,
+    height: 40,
+  },
+
+  shoppingcart: {
+    flex: 1,
+    flexDirection: 'column',
+    flexGrow: 0, 
+    flexShrink: 0,
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginRight: 55,
+  },
+
+  icon: {
+    height: 25,
+    width: 25,
+  }, 
+
+  text: {
+    marginTop: 5,
+    marginLeft: 5,
+  }
 });
 
 //hallo
